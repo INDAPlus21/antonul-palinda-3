1fee3f8925d2ce87f90e73f1e2335387e5788d9b
Anton U <antonul@kth.se>
2022-04-06 13:39:27 +0200

    Mapreduce

--- src/mapreduce/words.go
+++ src/mapreduce/words.go
@@ -2,18 +2,56 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+	"sync"
 	"time"
 )
 
 const DataFile = "loremipsum.txt"
 
 // Return the word frequencies of the text argument.
-//
-// Split load optimally across processor cores.
 func WordCount(text string) map[string]int {
-	freq := make(map[string]int)
-	// ...
-	return freq
+	words := strings.Fields(text)
+	freqs := make(map[string]int)
+
+	amount := len(words)
+
+	//Approx 1 ms per goroutine
+	routineSize := 1
+
+	freq_chan := make(chan map[string]int, amount/routineSize+1)
+
+	wg := new(sync.WaitGroup)
+
+	for i, j := 0, routineSize; j < amount+1; i, j = j, j+routineSize {
+
+		if j > amount {
+			j = amount
+		}
+
+		wg.Add(1)
+		go func(i, j int) {
+			freq := make(map[string]int)
+			for k := i; k < j; k++ {
+				word := strings.ToLower(words[k])
+				word = strings.ReplaceAll(word, ".", "")
+				word = strings.ReplaceAll(word, ",", "")
+				freq[word] += 1
+			}
+			freq_chan <- freq
+			wg.Done()
+		}(i, j)
+	}
+	wg.Wait()
+	close(freq_chan)
+	for something := range freq_chan {
+		for k, v := range something {
+			freqs[k] += v
+		}
+	}
+	return freqs
 }
 
 // Benchmark how long it takes to count word frequencies in text numRuns times.
@@ -39,8 +77,14 @@ func printResults(runtimeMillis int64, numRuns int) {
 
 func main() {
 	// read in DataFile as a string called data
+	data, err := ioutil.ReadFile("loremipsum.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	numRuns := 100
+	fmt.Printf("%#v", WordCount(string(data)))
+
+	numRuns := 1
 	runtimeMillis := benchmark(string(data), numRuns)
 	printResults(runtimeMillis, numRuns)
 }
